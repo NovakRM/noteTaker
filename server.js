d33c4f245ff377dff72a5ea833739d157648f392
osteophagy <mnovakahs@gmail.com>
2021-02-15 12:47:04 -0500

    working on getting content to render. not sure what's tripping it.

--- server.js
+++ server.js
@@ -1,50 +1,82 @@
-//Add Dependencies (incl Nodemon for hot refresh)
-const express = require("express")
-// const nodemon = require ("nodemon")
-const path = require("path")
-const fs = require("fs")
-const db = require("./db/db.json")
+// Dependencies
+let express = require("express");
+let path = require("path");
+let fs = require('fs');
+const { json } = require("express");
 
-let app = express()
-let PORT = process.env.PORT || 3000 //dynamic port
+// initiate Express and set the Port 
+var app = express();
+var PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+// connect express to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-//Paths - Get
-app.get("/", (req, res)=>{
-    res.sendFile(path.join(__dirname, "./public/index.html"))
-}) //both / and /index route to homepage
 
-app.get("/index", (req, res)=>{
-    res.sendFile(path.join(__dirname, "./public/index.html"))
-})
+//Routing 
+//GETs 
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
 
-app.get("/notes", (req, res)=>{
-    res.sendFile(path.join(__dirname, "./public/notes.html"))
-}) // route to notes
+app.get("/index", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
 
-app.get("/api/notes", (req, res)=>{
-    res.sendFile(path.join(__dirname, "./db/db.json"))
-}) //object containing saved notes
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+  });
 
-//Paths - Post
-app.post("/api/notes", (req, res)=>{
-    let newNote = req.body //newNote = content sent by client
-    newNote.id = newNote.id.replace(/\s+/g, "").toLowerCase() //gives note an id to be grabbed by
-    fs.readFile("./db/db.json", "utf-8", (err, data)=>{ //utf-8 eliminates the need for server-side logic to individually determine charEnc for each incoming form submission.
-        let oldNote = JSON.parse(data) //convert json string to object so new note can be pushed in
-        oldNote.push(newNote) //push newnote to oldnote
-        fs.writeFile("./db/db.json", JSON.stringify(oldNote), ()=>{}) //overwrite db with newly pushed item
-        res.json(newNote) //sends JSON response
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./db/db.json"));
+  });
+
+app.get("public/assets/js/index.js", function(req, res) {
+    res.sendFile(path.join(__dirname, "./assets/js/index.js"));
+  });
+
+app.get("/public/assets/css/styles.css", function(req, res) {
+    res.sendFile(path.join(__dirname, "./assets/css/styles.css"));
+  });
+
+//POSTs
+app.post("/api/notes", function(req, res) {
+    let newNote = req.body;
+    console.log(newNote);
+    newNote.id = newNote.title.replace(/\s+/g, "").toLowerCase()
+    fs.readFile("./db/db.json", 'utf-8',(err,data)=>{
+      let oldNote = JSON.parse(data)
+      //console.log(oldNote)
+      oldNote.push(newNote)
+      fs.writeFile("./db/db.json", JSON.stringify(oldNote), ()=>{})
+      res.json(newNote);
     })
-})
 
-//Paths - Delete
-// app.delete("/api/notes/:id", (req, res)=>{
-//     res.send
-// })
+  });
 
-app.listen(PORT, ()=>{
-    console.log(`app listening @ ${PORT}`)
-})
+//DELETEs
+app.delete("/api/notes/:id", function(req,res){
+    let id = req.params.id;
+    fs.readFile("./db/db.json", 'utf-8',(err,data)=>{
+      let notesArray = JSON.parse(data)
+      
+      for (let i=0; i < notesArray.length; i++){
+        if (notesArray.id !== id){
+          notesArray.splice(i, 1)
+          }
+        }
+        fs.writeFile("./db/db.json", JSON.stringify(notesArray), ()=>{})
+        res.json(notesArray)
+      });
+    })
+    
+
+  
+
+
+
+  //Start server listening
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
